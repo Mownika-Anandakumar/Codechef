1fd90e1475f8d39f2f90f6c572c48fa20766de23
Mownika Anandakumar <165825348+Mownika-Anandakumar@users.noreply.github.com>
2025-02-01 15:03:32 +0530

    Create variables in java.java

---
+++ variables in java.java
@@ -0,0 +1,6 @@
+class Main {
+    public static void main(String[] args) {
+        int age = 25; // Declare and assign the variable
+        System.out.println(age); // Print the value of age
+    }
+}
